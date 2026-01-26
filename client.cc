2f3c5b70c2edd9cd67f6c4ab5ae5e050df9bcaa9
shemeshg <gili.shemesh@gmail.com>
2026-01-26 23:43:11 +0200

    commit

--- client.cc
+++ client.cc
@@ -9,17 +9,6 @@ int main() {
     double five = c.call("add", 2, 3).as<double>();
     std::cout << five << std::endl;
 
-    std::cout << "sub(3, 2) = ";
-    double one = c.call("sub", 3, 2).as<double>();
-    std::cout << one << std::endl;
-
-    std::cout << "mul(5, 0) = ";
-    double zero = c.call("mul", five, 0).as<double>();
-    std::cout << zero << std::endl;
-
-    std::cout << "div(3, 0) = ";
-    double hmm = c.call("div", 3, 0).as<double>();
-    std::cout << hmm << std::endl;
 
     return 0;
 }
--- server.cc
+++ server.cc
@@ -1,26 +1,12 @@
-#include "rpc/server.h"
-
-double divide(double a, double b) { return a / b; }
-
-struct subtractor {
-    double operator()(double a, double b) { return a - b; }
-};
-
-struct multiplier {
-    double multiply(double a, double b) { return a * b; }
-};
+#include <rpc/server.h>
+#include <libSMngr.h>
 
 int main() {
     rpc::server srv(8080);
 
-    subtractor s;
-    multiplier m;
-
-    srv.bind("add", [](double a, double b) { return a + b; });
-    srv.bind("sub", s);
-    srv.bind("div", &divide);
-    srv.bind("mul", [&m](double a, double b) { return m.multiply(a, b); });
-
+    
+    srv.bind("add", [](double a, double b) { return add(a,b); });
+    
     srv.run();
 
     return 0;
