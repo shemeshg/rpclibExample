054f91d16efacefc01108bdfe33a0460c21c4256
shemeshg <gili.shemesh@gmail.com>
2026-01-27 21:09:06 +0200

    commit

--- SMngr/libSMngr/libSMngr.h
+++ SMngr/libSMngr/libSMngr.h
@@ -1,6 +1,22 @@
 #pragma once
 #include <string>
 
+class CounterExampleServer {
+    public:
+    explicit CounterExampleServer(int initialValue):val{initialValue}{
+
+    }
+    void add(int i){
+        this->val += i;
+    }
+
+    int get(){
+        return this->val;
+    }
+
+    private:
+    int val;
+};
 
 int add(int a, int b);
 
--- serverBal/serverBal.h
+++ serverBal/serverBal.h
@@ -13,6 +13,33 @@ public:
                  { return add(a, b); });
         srv.bind("stop", []()
                  { rpc::this_server().stop(); });
+        srv.bind("CounterExampleServerinit", [this](std::string uuid, int initialValue)
+                 { sessionState.emplace(uuid, CounterExampleServer(initialValue)); });
+        srv.bind("CounterExampleServerAdd",
+                 [this](const std::string &uuid, int val) -> void
+                 {
+                     auto ptr = getSessionObj<CounterExampleServer>(uuid);
+                     ptr->add(val);
+                 });
+        srv.bind("CounterExampleServerGet",
+                 [this](const std::string &uuid, int val) -> int
+                 {
+                     auto ptr = getSessionObj<CounterExampleServer>(uuid);
+                     return ptr->get();
+                 });
+        srv.bind("eraseSessionState",
+                 [this](const std::string &uuid)
+                 {
+                     auto it = sessionState.find(uuid);
+                     if (it != sessionState.end())
+                     {
+                         sessionState.erase(it); // destructor of CounterExampleServer runs here
+                     }
+                     else
+                     {
+                         throw std::runtime_error("Session '" + uuid + "' not found");
+                     }
+                 });
     }
 
     void start()
@@ -20,11 +47,35 @@ public:
         srv.run();
     }
 
-    void startAsync(){
+    void startAsync()
+    {
         std::size_t thread_count = std::thread::hardware_concurrency();
         srv.async_run(thread_count);
     }
 
 private:
     rpc::server srv;
+    std::unordered_map<
+        std::string,
+        std::variant<CounterExampleServer>>
+        sessionState;
+
+    template <typename T>
+    T *getSessionObj(std::string uuid)
+    {
+        auto it = this->sessionState.find(uuid);
+        if (it == sessionState.end())
+        {
+            throw std::runtime_error("Unexpected: session '" + uuid + "' not found");
+        }
+        auto &var = it->second;
+
+        auto ptr = std::get_if<T>(&var);
+        if (!ptr)
+        {
+            throw std::runtime_error("Unexpected: session '" + uuid +
+                                     "' does not contain CounterExampleServer");
+        }
+        return ptr;
+    }
 };
