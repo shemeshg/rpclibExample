beba6c3816e75d2c594c3363da15ce0b3c1b9a34
shemeshg <gili.shemesh@gmail.com>
2026-01-27 17:14:00 +0200

    commit

--- clientBal/clientBal.h
+++ clientBal/clientBal.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <rpc/client.h>
-
+#include <rpc/rpc_error.h>
 template <typename T>
 class AsyncData { 
     public:
@@ -18,20 +18,35 @@ class AsyncData {
 
 class ClientBal {
     public:
-    ClientBal(std::string hostName, uint16_t hostPort):c{hostName, hostPort}{        
+    ClientBal(std::string hostName, uint16_t hostPort):c{hostName, hostPort}{   
+        c.set_timeout(1000);     
     }
 
     template <typename T>
-    AsyncData<T> add(T a, T b){        
+    AsyncData<T> add(T a, T b){    
+        validateConnection();    
         std::future<RPCLIB_MSGPACK::object_handle> a_future = c.async_call("add", a, b);
         AsyncData<T> ad(std::move(a_future));
         return ad;
     }
     
     void serverStop() {
+        validateConnection();
         c.call("stop");
     }
 
     private:
         rpc::client c;
+        void validateConnection(){
+            if (c.get_connection_state() == rpc::client::connection_state::connected){
+                return;
+            }
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(1s);
+            if (c.get_connection_state() != rpc::client::connection_state::connected){
+                    throw rpc::system_error(std::make_error_code(std::errc::connection_aborted),
+                                            "Client disconnected");
+
+            }
+        }
 }; 
