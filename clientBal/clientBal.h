b98c97732be97ff365aa8ef50fb7c54749bcae09
shemeshg <gili.shemesh@gmail.com>
2026-01-29 01:26:12 +0200

    commit

--- clientBal/clientBal.h
+++ clientBal/clientBal.h
@@ -1,53 +1,10 @@
 #pragma once
 #include <rpc/client.h>
 #include <rpc/rpc_error.h>
-template <typename T>
-class AsyncData
-{
-public:
-    explicit AsyncData(std::future<RPCLIB_MSGPACK::object_handle> &&fut) : a_future(std::move(fut))
-    {
-    }
 
-    T get()
-    {
-        return a_future.get().template as<T>();
-    }
+#include "AsyncData.h"
+#include "CounterExample.h"
 
-private:
-    std::future<RPCLIB_MSGPACK::object_handle> a_future;
-};
-
-class CounterExample {
-    public:
-    CounterExample(const CounterExample&) = delete;
-    CounterExample& operator=(const CounterExample&) = delete;
-
-    explicit CounterExample(rpc::client *c, int initialValue):
-    c{c}, uuid{c->call("getUuid").as<std::string>()}
-    {
-        c->call("CounterExampleServerinit",uuid, initialValue);
-    }
-    void add(int i){
-        c->call("CounterExampleServerAdd",uuid, i);
-    }
-
-    void setExpiredAt(int i){
-        c->call("CounterExampleServerExpiredAt",uuid, i);
-    }
-
-    int get(){
-        return c->call("CounterExampleServerGet", uuid).as<int>();
-    }
-
-    virtual ~CounterExample(){
-        c->call("sessionStateErase",uuid);
-    }
-
-    private:
-    rpc::client *c;
-    std::string uuid;
-};
 
 class ClientBal
 {
@@ -60,8 +17,7 @@ public:
     }
 
     void setClientTimeout(int i){
-        clientTimeout = i;
-        c.set_timeout(clientTimeout);
+        c.set_timeout(i);
     }
 
     template <typename T>
@@ -91,7 +47,7 @@ public:
 
 private:
     rpc::client c;
-    int clientTimeout = 1000;
+
     const int initialTimeToConnect = 500;
     void validateConnection()
     {
